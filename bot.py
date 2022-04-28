b5f0865b4548d0e2ae8bbcea2b745a7eb93b92a8
Juan Escalada <juanescalada175@gmail.com>
2022-04-28 14:57:19 -0700

    Add message and image logger

--- bot.py
+++ bot.py
@@ -55,19 +55,47 @@ async def oppai(msg):
     responses_oppai = [
         'Wanna see my oppai, master?',
         '(o)(o)',
-        'Oppai oppai!']
+        'Oppai oppai!',
+        'Are you tired? You can touch my oppai, Master.']
+    #
+    # responses = [
+    #     'Hello, Master!',
+    #     'Can I help you, Master?',
+    #     "Come a little closer, I can't hear you from here <3"
+    # ]
+    response = random.choice(responses_oppai)
+    await msg.channel.send(response)
 
-    responses = [
-        'Hello, Master!',
-        'Can I help you, Master?',
-        "Come a little closer, I can't hear you from here <3"
-    ]
 
-    if 'oppai' not in message.content:
-        response = random.choice(responses)
-    else:
-        response = random.choice(responses_oppai)
-    await message.channel.send(response)
+@bot.event
+async def on_message(msg):
+    if msg.attachments:
+        appender('oppai-img.log', f'{msg.author}: {msg.content}')
+        for i, attachment in enumerate(msg.attachments):
+            appender('oppai-img.log', f'Attachment {i+1}: {attachment.filename}: {attachment.proxy_url}')
+    appender('oppai.log', f'{msg.author}: {msg.content}')
+    if re.search("^[Oo]ppai(| |-)[Cc]han.*\\?$", msg.content):
+        n = random.random()
+        yes_responses = ["Of course, Master!", "I think so, Master.", "Yes, master!", "I'm sure of it, Master!"]
+        no_responses = ["I don't think so, Master!", "Definitely not, Master.", "No, master!", "Probably not, Master!"]
+        if n > 0.5:
+            response = random.choice(yes_responses)
+        else:
+            response = random.choice(no_responses)
+        await msg.channel.send(response)
+    elif re.search("^おっぱいちゃん.*[?|？]$", msg.content):
+        n = random.random()
+        yes_responses = ["もちろんです、ご主人様", "そう思います、ご主人様", "はい、ご主人様!", "必ず、ご主人様"]
+        no_responses = ["そう思いません、ご主人様", "いいえ、ご主人様", "そんなことはありません、ご主人様"]
+        if n > 0.5:
+            response = random.choice(yes_responses)
+        else:
+            response = random.choice(no_responses)
+        await msg.channel.send(response)
+    elif re.search("!game", msg.content):
+        response = "Let's play a game, master!"
+        await msg.channel.send(response)
+
 
 @bot.event
 async def on_error(event, *args, **kwargs):
