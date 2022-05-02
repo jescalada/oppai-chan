fd56d8337d4074cd1a2bb53d2f76e89aef55a35b
Juan Escalada <juanescalada175@gmail.com>
2022-05-01 17:56:06 -0700

    Modularize on_message functions

--- bot.py
+++ bot.py
@@ -43,18 +43,6 @@ async def on_member_join(member):
     )
 
 
-@bot.command(name='yn', help='Simulates a yes or no.')
-async def roll(ctx):
-    n = random.random()
-    yes_responses = ["Of course, Master!", "I think so, Master.", "Yes, master!", "I'm sure of it, Master!"]
-    no_responses = ["I don't think so, Master!", "Definitely not, Master.", "No, master!", "Probably not, Master!"]
-    if n > 0.5:
-        response = random.choice(yes_responses)
-    else:
-        response = random.choice(no_responses)
-    await ctx.send(response)
-
-
 @bot.command(name='oppai')
 async def oppai(msg):
     responses_oppai = [
@@ -74,41 +62,60 @@ async def oppai(msg):
 
 @bot.event
 async def on_message(msg):
-    # If the message has attachments
+    # If the message has attachments, logs them
     if msg.attachments:
-        appender('oppai-img.log', f'{msg.author}: {msg.content}')
-        for i, attachment in enumerate(msg.attachments):
-            appender('oppai-img.log', f'Attachment {i+1}: {attachment.filename}: {attachment.proxy_url}')
+        log_attachments(msg)
+    # Logs message author and contents
     appender('oppai.log', f'{msg.author}: {msg.content}')
+    # Uses regex to respond to various commands
     if re.search("^[Oo]ppai(| |-)[Cc]han.*\\?$", msg.content):
-        n = random.random()
-        yes_responses = ["Of course, Master!", "I think so, Master.", "Yes, master!", "I'm sure of it, Master!"]
-        no_responses = ["I don't think so, Master!", "Definitely not, Master.", "No, master!", "Probably not, Master!"]
-        if n > 0.5:
-            response = random.choice(yes_responses)
-        else:
-            response = random.choice(no_responses)
-        await msg.channel.send(response)
+        await ask_oppai(msg)
     elif re.search("^おっぱいちゃん.*[?|？]$", msg.content):
-        n = random.random()
-        yes_responses = ["もちろんです、ご主人様", "そう思います、ご主人様", "はい、ご主人様!", "必ず、ご主人様"]
-        no_responses = ["そう思いません、ご主人様", "いいえ、ご主人様", "そんなことはありません、ご主人様"]
-        if n > 0.5:
-            response = random.choice(yes_responses)
-        else:
-            response = random.choice(no_responses)
-        await msg.channel.send(response)
+        await ask_oppai_japanese(msg)
     elif re.search("!send", msg.content):
-        with open('img/oppaichan.jpg', mode='rb') as oppai_binary:
-            for member in msg.guild.members:
-                if member.bot:
-                    continue
-                print(f'Sending message to {member.name}...')
-                oppai_binary.seek(0)
-                buffer = BytesIO(oppai_binary.read())
-                await member.send(content="Master Maxwell told me to send you this picture!", file=discord.File(fp=buffer, filename='oppai.jpg'))
-        response = "Sent, master!"
-        await msg.channel.send(response)
+        await distribute_files(msg)
+
+
+def log_attachments(msg):
+    appender('oppai-img.log', f'{msg.author}: {msg.content}')
+    for i, attachment in enumerate(msg.attachments):
+        appender('oppai-img.log', f'Attachment {i + 1}: {attachment.filename}: {attachment.proxy_url}')
+
+
+async def ask_oppai(msg):
+    n = random.random()
+    yes_responses = ["Of course, Master!", "I think so, Master.", "Yes, master!", "I'm sure of it, Master!"]
+    no_responses = ["I don't think so, Master!", "Definitely not, Master.", "No, master!", "Probably not, Master!"]
+    if n > 0.5:
+        response = random.choice(yes_responses)
+    else:
+        response = random.choice(no_responses)
+    await msg.channel.send(response)
+
+
+async def ask_oppai_japanese(msg):
+    n = random.random()
+    yes_responses = ["もちろん、ご主人様", "そう思います、ご主人様", "はい、ご主人様!", "必ず、ご主人様"]
+    no_responses = ["そう思いません、ご主人様", "いいえ、ご主人様", "そんなことはありません、ご主人様"]
+    if n > 0.5:
+        response = random.choice(yes_responses)
+    else:
+        response = random.choice(no_responses)
+    await msg.channel.send(response)
+
+
+async def distribute_files(msg):
+    with open('img/oppaichan.jpg', mode='rb') as oppai_binary:
+        for member in msg.guild.members:
+            if member.bot:
+                continue
+            print(f'Sending message to {member.name}...')
+            oppai_binary.seek(0)
+            buffer = BytesIO(oppai_binary.read())
+            await member.send(content="Master Maxwell told me to send you this picture!",
+                              file=discord.File(fp=buffer, filename='oppai.jpg'))
+    response = "Sent, master!"
+    await msg.channel.send(response)
 
 
 @bot.event
